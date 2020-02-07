34a92602c0c53d3d9a42f1e29ce2f794df4a270e
Raúl Armendáriz - oficina <raul_ar_reyes@hotmail.com>
2020-02-07 11:19:03 -0600

    Updates

---
+++ precache-manifest.500c2d9e9bfbbbf281180a112b693ad8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9298863f5cdb291e690e357cda1670d2",
+    "url": "/minesweeperreact/index.html"
+  },
+  {
+    "revision": "2303e6a426c5665bac83",
+    "url": "/minesweeperreact/static/css/main.276b873d.chunk.css"
+  },
+  {
+    "revision": "672874e9c0677e530f9c",
+    "url": "/minesweeperreact/static/js/2.0287c3f0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/minesweeperreact/static/js/2.0287c3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2303e6a426c5665bac83",
+    "url": "/minesweeperreact/static/js/main.fba149da.chunk.js"
+  },
+  {
+    "revision": "8c809b708c288d20ee29",
+    "url": "/minesweeperreact/static/js/runtime-main.30e82a4a.js"
+  },
+  {
+    "revision": "e00e78bfe8394b3e90c2d38a2b736f46",
+    "url": "/minesweeperreact/static/media/click.e00e78bf.mp3"
+  },
+  {
+    "revision": "d16031584c17f5d64868185e0c5058e6",
+    "url": "/minesweeperreact/static/media/flag.d1603158.mp3"
+  },
+  {
+    "revision": "ad9b69c637eb4dbe52cc31447e3036f3",
+    "url": "/minesweeperreact/static/media/mine.ad9b69c6.mp3"
+  },
+  {
+    "revision": "ce93d662f34f36a06e1952b58217ad32",
+    "url": "/minesweeperreact/static/media/mine_show.ce93d662.mp3"
+  },
+  {
+    "revision": "cd6ac11e369c5338295bd735eb2b6d55",
+    "url": "/minesweeperreact/static/media/modal_score.cd6ac11e.mp3"
+  },
+  {
+    "revision": "f142672b7e2a6e242295127fe6b8f26c",
+    "url": "/minesweeperreact/static/media/show_zone.f142672b.mp3"
+  }
+]);
